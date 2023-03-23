da8ffd30527445f88d732e9965086efaeac0a4de
emilioff8822 <emilioff@hotmail.it>
2023-03-23 17:05:41 +0100

    commit-for-mail

--- mail/script.js
+++ mail/script.js
@@ -1,10 +1,40 @@
-const emailAutorizzate = ['emilioff@hotmail.it', 'francesca@hotmail.it',
+let emailAutorizzate = ['emilioff@hotmail.it', 'francesca@hotmail.it',
 'luigi@hotmail.it'
 ];
 
 const emailUtente = prompt('Inserisci la tua mail');
 
 const mess = document.getElementById('messaggio');
+
+let accesso = false;
+for (let i=0; i < emailAutorizzate.length; i++ ) {
+ 
+  if (emailUtente === emailAutorizzate[i]){
+
+    accesso = true;
+    mess.innerHTML= 'ACCESSO  AUTORIZZATO'
+    mess.classList.add('ok')
+  }
+
+  else{
+
+  mess.innerHTML = 'ACCESSO NON AUTORIZZATO';
+  mess.classList.add('no')
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*includes
 
 if (emailAutorizzate.includes(emailUtente)){
 mess.innerHTML= 'ACCESSO  AUTORIZZATO'
@@ -16,3 +46,5 @@ else {
   mess.innerHTML = 'ACCESSO NON AUTORIZZATO';
   mess.classList.add('no')
 }
+
+*/
