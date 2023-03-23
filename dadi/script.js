0c31e8f2c31206b2fec4a860d8260f443393e138
emilioff8822 <emilioff@hotmail.it>
2023-03-23 14:44:13 +0100

    commit-dadi

---
+++ dadi/script.js
@@ -0,0 +1,13 @@
+//genero un numero casuale da 1 a 6 per giocatore e computer
+
+let giocatore = Math.floor(Math.random() * 6) + 1;
+
+let computer = Math.floor(Math.random() * 6) + 1;
+
+// vedo in console i punteggi ottenuti
+
+console.log ("Il giocatore ha ottenuto un punteggio di " +  giocatore)
+
+console.log ("Il computer ha ottenuto un punteggio di " +  computer)
+
+
